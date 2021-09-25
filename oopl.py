20a5977f461358abb505a9f563caa08692879255
Chris Olah <31199188+Colah2122@users.noreply.github.com>
2021-09-24 23:44:22 -0400

    Add files via upload
    Completed Task#2 - Writing a pretty-printer for J0 programs

--- oopl.py
+++ oopl.py
@@ -1,29 +1,45 @@
+
 from abc import ABC, abstractmethod
 
 class JExpr(ABC):
-    pass
+    def ppt(self):
+        return self.__str__();
+    @abstractmethod
+    def __str__(self):
+        pass
 
 class JNum(JExpr):
     def __init__(self, eNum):
         self.eNum = int(eNum)
+    def __str__(self):
+        return str(self.eNum)
 
 class JPlus(JExpr):
     def __init__(self, eLeft, eRight):
         self.eLeft = eLeft
         self.eRight = eRight
+    def __str__(self):
+        return "(+ " + str(self.eLeft) + " " + str(self.eRight) + ")"
 
 class JMult(JExpr):
     def __init__(self, eLeft, eRight):
         self.eLeft = eLeft
         self.eRight = eRight
+    def __str__(self):
+        return "(* " + str(self.eLeft) + " " + str(self.eRight) + ")"
 
 
 print()
 print("="*80)
-print(">"*8, "task 1: Define data structures to represent J0 programs")
+print(">"*8, "task 2: Write a pretty-printer for J0 programs")
 print("="*80)
 
 a = JNum(3)
 b = JPlus(JNum(3),JNum(5))
 c = JMult(JNum(-3),JNum(-2))
 d = JPlus(JMult(JNum(-4),JNum(5)),JMult(JNum(2),JNum(12)))
+
+print("a=",a.ppt())
+print("b=",b.ppt())
+print("c=",c.ppt())
+print("d=",d.ppt())
