c47658bee013df068e0c5171cbdcd2aa5547c3c9
Chris Olah <31199188+Colah2122@users.noreply.github.com>
2021-09-24 23:51:18 -0400

    Add files via upload
    Completed "Task #5 - Defining a data structure to represent Sexprs"

--- oopl.py
+++ oopl.py
@@ -44,26 +44,11 @@ def JCheck(e, expAns):
     print(e.ppt(), " = ", actAns, " >>> ", result)
     return isCorrect
 
-e = []
-e.append([JNum(9), 9])
-e.append([JPlus(JNum(2),JNum(3)), 5])
-e.append([JMult(JNum(2),JNum(7)), 14])
-e.append([JPlus(JNum(1),JMult(JNum(2),JNum(5))), 11])
-e.append([JPlus(JNum(-3),JMult(JNum(-1),JNum(9))), -12])
-e.append([JPlus(JNum(-114),JPlus(JNum(1),JMult(JNum(-2),JNum(-5)))), -103])
-e.append([JMult(JNum(14),JPlus(JNum(1),JPlus(JNum(2),JNum(5)))), 112])
-e.append([JPlus(JNum(3),JPlus(JNum(2),JPlus( JMult(JNum(3),JNum(4)),JMult(JNum(-1),JNum(4))))), 13])
-e.append([JPlus(JNum(5),JPlus(JNum(1),JMult(JNum(2),JMult(JNum(7),JNum(5))))), 76])
-e.append([JPlus(JMult(JNum(2),JNum(3)),JPlus(JPlus(JPlus(JNum(2),JNum(-4)),JNum(2)),JMult(JNum(2),JNum(3)))), 12])
-e.append([JMult(JPlus(JNum(1),JMult(JNum(2),JNum(5))),JMult(JNum(14),JPlus(JNum(1),JPlus(JNum(2),JNum(5))))), 1232])
-e.append([JPlus(JMult(JNum(2),JNum(3)),JPlus(JPlus(JPlus(JPlus(JNum(-3),JMult(JNum(-1),JNum(9))),JNum(-4)),JNum(2)),JMult(JNum(2),JNum(3)))), -2])
-
 
 print()
 print("="*80)
-print(">"*8, "task 4: Implement a big-step interpreter for J0")
+print(">"*8, "task 5: Define a data structure to represent Sexprs")
 print("="*80)
 
-for l in e:
-    print("-"*50)
-    JCheck(l[0], l[1])
+se = list(["+", "4", ["+", 3, 4 ]])
+print("se=",se)
